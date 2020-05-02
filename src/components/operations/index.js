a68681bda2579ad2acad6066469e15b29d362d31
wptechprodigy <waheed.freelancer16@gmail.com>
2020-05-02 18:52:18 +0100

    add real-time fetch functionality

--- src/components/operations/index.js
+++ src/components/operations/index.js
@@ -1,10 +1,52 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import requestPromise from 'request-promise';
 import Operation from './Operation';
 
 function Operations() {
+  const [data, setData] = useState([]);
+  const [isLoading, setIsLoading] = useState(false)
+  const [isError, setIsError] = useState(false);
+
+	useEffect(() => {
+    const requestServiceStatus = async () => {
+      setIsLoading(true);
+      setIsError(false);
+
+      const options = {
+				uri: 'https://www.githubstatus.com/',
+				json: true,
+      };
+
+      try {
+        const response = await requestPromise(options);
+
+        setData(response.components);
+      } catch (error) {
+        setIsError(true);
+      }
+
+      setIsLoading(false);
+    }
+
+    requestServiceStatus();
+  }, []);
+
 	return (
 		<div className='flex flex-col m-8 md:flex flex-grow'>
-			<Operation />
+			{isError && (
+				<h1 className='text-center font-black text-3xl'>
+					Something went wrong{' '}
+					<span role='img' aria-label='Error occurred'>
+						😪
+					</span>
+				</h1>
+			)}
+
+			{isLoading ? (
+				<h1 className='text-center font-black text-3xl'>Loading...</h1>
+			) : (
+				data.map(({id, ...restData }) => <Operation key={id} restData={restData} />)
+			)}
 		</div>
 	);
 }
