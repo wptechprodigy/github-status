3f8c8069f5058e22aa8c82e7999d2479561ce3d6
wptechprodigy <waheed.freelancer16@gmail.com>
2020-05-02 16:22:07 +0100

    add an operation component

---
+++ src/components/operations/Operation.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function Operation() {
+	return (
+		<div className='flex justify-between md:flex md:justify-between bg-white rounded-lg p-6 mx-auto mb-2 shadow w-5/6'>
+			<span className='w-3/4 font-bold text-2xl text-gray-700'>
+				GitHub Packages
+			</span>
+			<span className='w-1/4 bg-green-600 rounded-full text-white text-center py-1 pb-2 font-light border-solid border-4'>
+				Operational
+			</span>
+		</div>
+	);
+}
+
+export default Operation;
