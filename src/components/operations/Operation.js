5d511f061c9ad1f8f542572d39769248c8b5452d
wptechprodigy <waheed.freelancer16@gmail.com>
2020-05-02 18:51:54 +0100

    make each operation dynamic

--- src/components/operations/Operation.js
+++ src/components/operations/Operation.js
@@ -1,14 +1,17 @@
 import React from 'react';
 
-function Operation() {
+function Operation({restData: {name, status, description}}) {
 	return (
 		<div className='flex justify-between md:flex md:justify-between bg-white rounded-lg p-6 mx-auto mb-2 shadow w-5/6'>
-			<span className='w-3/4 font-bold text-2xl text-gray-700'>
-				GitHub Packages
-			</span>
-			<span className='w-1/4 bg-green-600 rounded-full text-white text-center py-1 pb-2 font-light border-solid border-4'>
-				Operational
-			</span>
+			<div className='w-3/4 font-bold text-xl text-gray-700'>
+				{name}
+      <span className='block text-gray-700 font-medium text-sm'>
+        {description}
+      </span>
+			</div>
+			<div className='w-1/4 bg-green-600 rounded-full text-white text-center my-auto pt-1 pb-2 font-light border-solid border-4'>
+				{status}
+			</div>
 		</div>
 	);
 }
