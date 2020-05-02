666d49757fa4f0fc55e510ebf3d98520b5f8829f
wptechprodigy <waheed.freelancer16@gmail.com>
2020-05-02 16:23:00 +0100

    update app with the new components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import Header from './components/header';
 import Footer from './components/footer';
+import Operations from './components/operations';
 
 function App() {
 	return (
-		<div className='flex flex-col min-h-screen'>
+		<div className='flex flex-col min-h-screen bg-green-100'>
       <Header />
-			<h1 className='font-bold text-purple-700 flex-grow'>Hello world!</h1>
+			<Operations />
       <Footer />
 		</div>
 	);
