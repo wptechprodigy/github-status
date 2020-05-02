a3cf5f449c5701bdfa8e38fd273b7cd64af72146
wptechprodigy <waheed.freelancer16@gmail.com>
2020-05-02 10:35:58 +0100

    add header to app

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React from 'react';
+import Header from './components/header';
 
 function App() {
 	return (
-		<div className='align-middle'>
+		<div className='grid grid-cols-1'>
+      <Header />
 			<h1 className='font-bold text-purple-700'>Hello world!</h1>
 		</div>
 	);
